2bea245d15b1a5994e4d83747c053e161ffd4c48
ErmiasVentures <etesgayventures@gmail.com>
2026-07-11 15:00:14 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-
-const m=document.querySelector('.menu'),n=document.querySelector('.links');
-if(m&&n){m.onclick=()=>n.classList.toggle('open');n.querySelectorAll('a').forEach(a=>a.onclick=()=>n.classList.remove('open'))}
-document.querySelectorAll('[data-year]').forEach(e=>e.textContent=new Date().getFullYear());
